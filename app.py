5e88e56e43a2e28ccd99892e070ab9b687329f68
Aaham <aahamkumararya1@gmail.com>
2026-05-04 10:57:03 +0530

    final deploy

--- app.py
+++ app.py
@@ -848,7 +848,11 @@ def internal_error(error):
     db.session.rollback()
     return render_template('errors/500.html'), 500
 
+import os
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
